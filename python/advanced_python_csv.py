246b6c6496a5bdb40574d9025cad42b38452ae8a
jjchoi08 <jjchoi08@gmail.com>
2017-01-08 19:39:08 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+'''
+Part II - Write to CSV File
+Q5. Write email addresses from Part I to csv file
+
+Place your code in this file: advanced_python_csv.py
+
+The emails.csv file you create should be added and committed to your forked repository.
+
+Your file, emails.csv, will look like this:
+
+bellamys@mail.med.upenn.edu
+warren@upenn.edu
+bryanma@upenn.edu
+'''
+
