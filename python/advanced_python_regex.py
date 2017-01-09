00bdc782d1ea614d4e501aa98360c9ac4d82e88e
jjchoi08 <jjchoi08@gmail.com>
2017-01-08 19:38:25 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+'''
+Q1. Find how many different degrees there are, and their frequencies: Ex: PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
+
+        REPLACE THIS WITH YOUR RESPONSE
+
+Q2. Find how many different titles there are, and their frequencies: Ex: Assistant Professor, Professor
+
+        REPLACE THIS WITH YOUR RESPONSE
+
+Q3. Search for email addresses and put them in a list. Print the list of email addresses.
+
+        REPLACE THIS WITH YOUR RESPONSE
+
+Q4. Find how many different email domains there are (Ex: mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.). Print the list of unique email domains.
+
+        REPLACE THIS WITH YOUR RESPONSE
+'''
+
+
+
+
